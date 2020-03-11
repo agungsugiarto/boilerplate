f372d33c2e6d360e0cd9f7a2bfb1dfa154e6c13c
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-03-11 18:32:50 +0700

    damn conflict

--- src/Views/Role/edit.php
+++ src/Views/Role/edit.php
@@ -18,15 +18,7 @@
             </div>
             <div class="card-body">
                 <div class="row">
-<<<<<<< HEAD
                     <?= form_open('admin/role/update/'.$role->id, ['method' => 'post']) ?>
-=======
-<<<<<<< HEAD
-                    <?= form_open('admin/role/update/'.$role->id, ['method' => 'post']) ?>
-=======
-                    <?= form_open('admin/role/update', ['method' => 'put']) ?>
->>>>>>> 633cd2c19dd1354dec518e6845ee8ab61fb80114
->>>>>>> dd49c916b616eb0ddf98cef94908b11287f65d8a
                         <div class="col-md-10">
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label">Name</label>
