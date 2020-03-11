dae5db33f45f0acca101da572842991cb7cf55f8
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-03-11 20:58:30 +0700

    WIB: boilerplate

--- src/Database/Migrations/2020-02-03-081118_create_menu_table.php
+++ src/Database/Migrations/2020-02-03-081118_create_menu_table.php
@@ -10,7 +10,7 @@ class CreateMenuTable extends Migration
     {
         // TODO: menu item
         $this->forge->addField([
-            'id'         => ['type' => 'int', 'constraint' => 11, 'auto_increment' => true],
+            'id'         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
             'parent_id'  => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'null' => true],
             'active'     => ['type' => 'tinyint', 'constraint' => '1', 'default' => 1],
             'title'      => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
@@ -23,18 +23,19 @@ class CreateMenuTable extends Migration
         ]);
 
         $this->forge->addKey(['id', 'parent_id']);
+        $this->forge->addForeignKey('parent_id', 'menu', 'id', false, 'SET NULL');
         $this->forge->createTable('menu', true);
 
         // TODO: group menu item
         $this->forge->addField([
-            'id'       => ['type' => 'int', 'constraint' => 11, 'auto_increment' => true],
+            'id'       => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
             'group_id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
             'menu_id'  => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
         ]);
 
         $this->forge->addKey('id', 'group_id', 'menu_id');
-        $this->forge->addForeignKey('group_id', 'auth_groups', 'id', false, 'CASCADE');
         $this->forge->addForeignKey('menu_id', 'menu', 'id', false, 'CASCADE');
+        $this->forge->addForeignKey('group_id', 'auth_groups', 'id', false, 'CASCADE');
         $this->forge->createTable('groups_menu', true);
     }
 
