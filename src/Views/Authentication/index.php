68dd411cfc7f8ca70f28634dffd2696b96cfbe8c
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-03-11 21:10:46 +0700

    WIP: boilerplate

--- src/Views/Authentication/index.php
+++ src/Views/Authentication/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?= config('app')->defaultLocale ?>">
+<html lang="<?= config('App')->defaultLocale ?>">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- src/Views/Load/datatables.php
+++ src/Views/Load/datatables.php
@@ -9,7 +9,7 @@
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
 <script>
-    moment.locale('<?= config('app')->defaultLocale ?>');
+    moment.locale('<?= config('App')->defaultLocale ?>');
 </script>
 <script>
     $.extend( true, $.fn.dataTable.defaults, {
