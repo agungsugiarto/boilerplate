b1db2e51a1b1db977b49c3b27e769926a309240f
Agung Sugiarto <sugiartoagung92@gmail.com>
2020-04-14 11:31:26 +0700

    prepare new version 2.x

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,7 @@ mix.copy('resource/images/avatar.png', 'public/images/avatar.png', false);
 
 mix.scripts([
     'node_modules/moment/min/moment-with-locales.min.js',
-], 'public/js/moment/moment-with-locales.min.js').version();
+], 'public/plugin/moment/moment-with-locales.min.js').version();
 
 // ============== Datatables ==============
 
@@ -28,113 +28,113 @@ mix.scripts([
     'node_modules/drmonty-datatables-plugins/dataRender/datetime.js',
     'node_modules/drmonty-datatables-plugins/sorting/datetime-moment.js',
     'resource/assets/js/datatables.js',
-], 'public/js/datatables/datatables.min.js').version();
+], 'public/plugin/datatables/datatables.min.js').version();
 
-mix.copy('node_modules/drmonty-datatables-plugins/i18n', 'public/js/datatables/i18n/', false);
+mix.copy('node_modules/drmonty-datatables-plugins/i18n', 'public/plugin/datatables/i18n/', false);
 
 mix.styles(
     'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css',
-    'public/js/datatables/datatables.min.css'
+    'public/plugin/datatables/datatables.min.css'
 ).version();
 
 // ============== Select2 ==============
 
 mix.scripts([
     'node_modules/select2/dist/js/select2.full.min.js'
-], 'public/js/select2/select2.full.min.js').version();
+], 'public/plugin/select2/select2.full.min.js').version();
 
-mix.copy('node_modules/select2/dist/js/i18n', 'public/js/select2/i18n/', false);
+mix.copy('node_modules/select2/dist/js/i18n', 'public/plugin/select2/i18n/', false);
 
 
 // ============== DatePicker ==============
 
-mix.sass('resource/assets/scss/daterangepicker.scss', 'public/js/datepicker/datepicker.min.css').version();
+mix.sass('resource/assets/scss/daterangepicker.scss', 'public/plugin/datepicker/datepicker.min.css').version();
 
 mix.scripts([
     'node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.js',
     'node_modules/admin-lte/plugins/daterangepicker/daterangepicker.js',
-], 'public/js/datepicker/datepicker.min.js').version();
+], 'public/plugin/datepicker/datepicker.min.js').version();
 
 // ============== FileInput ==============
 
 mix.sass(
     'node_modules/bootstrap-fileinput/scss/fileinput.scss',
-    'public/js/fileinput/bootstrap-fileinput.min.css'
+    'public/plugin/fileinput/bootstrap-fileinput.min.css'
 ).version();
 
 mix.scripts([
     'node_modules/bootstrap-fileinput/js/fileinput.min.js',
-], 'public/js/fileinput/bootstrap-fileinput.min.js').version();
+], 'public/plugin/fileinput/bootstrap-fileinput.min.js').version();
 
-mix.copy('node_modules/bootstrap-fileinput/js/locales', 'public/js/fileinput/locales', false);
+mix.copy('node_modules/bootstrap-fileinput/js/locales', 'public/plugin/fileinput/locales', false);
 
 // ======= Code Mirror
-mix.scripts(['node_modules/codemirror/lib/codemirror.js'], 'public/js/codemirror/codemirror.min.js').version();
-mix.scripts(['resource/assets/js/vendor/codemirror/jquery.codemirror.js'], 'public/js/codemirror/jquery.codemirror.min.js').version();
+mix.scripts(['node_modules/codemirror/lib/codemirror.js'], 'public/plugin/codemirror/codemirror.min.js').version();
+mix.scripts(['resource/assets/js/vendor/codemirror/jquery.codemirror.js'], 'public/plugin/codemirror/jquery.codemirror.min.js').version();
 
-mix.copy('node_modules/codemirror/addon', 'public/js/codemirror/addon');
-mix.copy('node_modules/codemirror/mode', 'public/js/codemirror/mode');
-mix.copy('node_modules/codemirror/theme', 'public/js/codemirror/theme');
+mix.copy('node_modules/codemirror/addon', 'public/plugin/codemirror/addon');
+mix.copy('node_modules/codemirror/mode', 'public/plugin/codemirror/mode');
+mix.copy('node_modules/codemirror/theme', 'public/plugin/codemirror/theme');
 
-mix.sass('resource/assets/js/vendor/codemirror/theme/storm.scss', 'public/js/codemirror/theme/storm.css');
+mix.sass('resource/assets/js/vendor/codemirror/theme/storm.scss', 'public/plugin/codemirror/theme/storm.css');
 
-mix.styles('node_modules/codemirror/lib/codemirror.css', 'public/js/codemirror/codemirror.min.css').version();
+mix.styles('node_modules/codemirror/lib/codemirror.css', 'public/plugin/codemirror/codemirror.min.css').version();
 
 // ============== TinyMCE ==============
 
-mix.copy('node_modules/tinymce/plugins', 'public/js/tinymce/plugins');
-mix.scripts('resource/assets/js/vendor/tinymce/plugins/codemirror/plugin.js', 'public/js/tinymce/plugins/codemirror/plugin.min.js');
-mix.scripts('resource/assets/js/vendor/tinymce/plugins/customalign/plugin.js', 'public/js/tinymce/plugins/customalign/plugin.min.js');
-mix.copy('resource/assets/js/vendor/tinymce/plugins', 'public/js/tinymce/plugins');
-mix.copy('node_modules/tinymce/skins', 'public/js/tinymce/skins');
-mix.copy('node_modules/tinymce/themes', 'public/js/tinymce/themes');
-mix.copy('node_modules/stickytoolbar/dist', 'public/js/tinymce/plugins');
+mix.copy('node_modules/tinymce/plugins', 'public/plugin/tinymce/plugins');
+mix.scripts('resource/assets/js/vendor/tinymce/plugins/codemirror/plugin.js', 'public/plugin/tinymce/plugins/codemirror/plugin.min.js');
+mix.scripts('resource/assets/js/vendor/tinymce/plugins/customalign/plugin.js', 'public/plugin/tinymce/plugins/customalign/plugin.min.js');
+mix.copy('resource/assets/js/vendor/tinymce/plugins', 'public/plugin/tinymce/plugins');
+mix.copy('node_modules/tinymce/skins', 'public/plugin/tinymce/skins');
+mix.copy('node_modules/tinymce/themes', 'public/plugin/tinymce/themes');
+mix.copy('node_modules/stickytoolbar/dist', 'public/plugin/tinymce/plugins');
 
 // Boilerplate skin
-mix.copy('resource/assets/js/vendor/tinymce/skins/boilerplate/fonts', 'public/js/tinymce/skins/ui/boilerplate/fonts');
-mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/content.inline.scss', 'public/js/tinymce/skins/ui/boilerplate/content.inline.min.css');
-mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/content.mobile.scss', 'public/js/tinymce/skins/ui/boilerplate/content.mobile.min.css');
-mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/content.scss', 'public/js/tinymce/skins/ui/boilerplate/content.min.css');
-mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/skin.scss', 'public/js/tinymce/skins/ui/boilerplate/skin.min.css');
-mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/skin.mobile.scss', 'public/js/tinymce/skins/ui/boilerplate/skin.mobile.min.css');
+mix.copy('resource/assets/js/vendor/tinymce/skins/boilerplate/fonts', 'public/plugin/tinymce/skins/ui/boilerplate/fonts');
+mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/content.inline.scss', 'public/plugin/tinymce/skins/ui/boilerplate/content.inline.min.css');
+mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/content.mobile.scss', 'public/plugin/tinymce/skins/ui/boilerplate/content.mobile.min.css');
+mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/content.scss', 'public/plugin/tinymce/skins/ui/boilerplate/content.min.css');
+mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/skin.scss', 'public/plugin/tinymce/skins/ui/boilerplate/skin.min.css');
+mix.sass('resource/assets/js/vendor/tinymce/skins/boilerplate/skin.mobile.scss', 'public/plugin/tinymce/skins/ui/boilerplate/skin.mobile.min.css');
 
 // https://www.tiny.cloud/get-tiny/language-packages/
-mix.copy('resource/assets/js/vendor/tinymce/langs', 'public/js/tinymce/langs');
+mix.copy('resource/assets/js/vendor/tinymce/langs', 'public/plugin/tinymce/langs');
 
 mix.scripts([
     'node_modules/tinymce/tinymce.min.js',
     'node_modules/tinymce/jquery.tinymce.min.js'
-], 'public/js/tinymce/tinymce.min.js').version();
+], 'public/plugin/tinymce/tinymce.min.js').version();
 
 // ============== Nestable ==============
 
 mix.styles(
     'node_modules/nestable2/jquery.nestable.css',
-    'public/js/nestable2/jquery.nestable.min.css'
+    'public/plugin/nestable2/jquery.nestable.min.css'
 ).version();
 
 mix.scripts([
     'node_modules/nestable2/jquery.nestable.js',
-], 'public/js/nestable2/jquery.nestable.min.css').version();
+], 'public/plugin/nestable2/jquery.nestable.min.css').version();
 
 // ============== Fontawesome iconpicker ==============
 
 mix.less(
     'node_modules/fontawesome-iconpicker/src/less/iconpicker.less',
-    'public/js/fontawesome-iconpicker/fontawesome-iconpicker.min.css'
+    'public/plugin/fontawesome-iconpicker/fontawesome-iconpicker.min.css'
 ).version();
 
 mix.scripts([
     'node_modules/fontawesome-iconpicker/src/js/iconpicker.js',
-], 'public/js/fontawesome-iconpicker/fontawesome-iconpicker.min.js').version();
+], 'public/plugin/fontawesome-iconpicker/fontawesome-iconpicker.min.js').version();
 
 // ============== Duallistbox ==============
 
 mix.styles(
     'node_modules/bootstrap4-duallistbox/src/bootstrap-duallistbox.css',
-    'public/js/bootstrap4-duallistbox/bootstrap-duallistbox.min.css'
+    'public/plugin/bootstrap4-duallistbox/bootstrap-duallistbox.min.css'
 ).version();
 
 mix.scripts([
     'node_modules/bootstrap4-duallistbox/src/jquery.bootstrap-duallistbox.js',
-], 'public/js/bootstrap4-duallistbox/bootstrap-duallistbox.min.js').version();
+], 'public/plugin/bootstrap4-duallistbox/bootstrap-duallistbox.min.js').version();
