0ce966567b9825efb54d7177ef8e70a84482efe7
rubensrocha <10297656+rubensrocha@users.noreply.github.com>
2020-05-26 18:04:06 -0300

    Bug fix with cache menu group

--- src/Controllers/Users/MenuController.php
+++ src/Controllers/Users/MenuController.php
@@ -68,6 +68,9 @@ class MenuController extends BaseController
         if (!$result) {
             return $this->fail($result, lang('boilerplate.menu.msg.msg_fail_order'));
         }
+        
+		//Delete cache
+		$this->deleteCacheMenu();
 
         return $this->respondUpdated($result, lang('boilerplate.menu.msg.msg_update'));
     }
@@ -122,6 +125,9 @@ class MenuController extends BaseController
         }
 
         $this->db->transCommit();
+        
+		//Delete cache
+		$this->deleteCacheMenu();
 
         return redirect()->back()->with('sweet-success', lang('boilerplate.menu.msg.msg_insert'));
     }
@@ -178,6 +184,9 @@ class MenuController extends BaseController
         }
 
         $this->db->transCommit();
+        
+		//Delete cache
+		$this->deleteCacheMenu();
 
         return $this->respondUpdated($menu, lang('boilerplate.menu.msg.msg_update'));
     }
@@ -217,8 +226,20 @@ class MenuController extends BaseController
     {
         if ($found = $this->menu->delete($id)) {
             return $this->respondCreated($found, lang('boilerplate.menu.msg.msg_delete'));
+            //Delete cache
+            $this->deleteCacheMenu();
         }
 
         return $this->failNotFound(lang('boilerplate.menu.msg.msg_get_fail'));
     }
+    
+    /*
+	* Delete cache group menu
+	*/
+	private function deleteCacheMenu()
+	{
+		if (cache(user()->id.'_group_menu')) {
+			cache()->delete(user()->id.'_group_menu');
+		}
+	}
 }
