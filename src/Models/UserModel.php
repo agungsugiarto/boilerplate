71212e56c7e634867f23be638680aa78ed63373f
Radoslav Georgiev <kpeu3u@gmail.com>
2023-02-18 11:52:01 +0200

    add first name and last name to user

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -8,11 +8,23 @@ use Myth\Auth\Models\UserModel as BaseModel;
 class UserModel extends BaseModel
 {
     public const ORDERABLE = [
-        1 => 'first_name',
-        2 => 'last_name',
-        3 => 'username',
-        4 => 'email',
+        1 => 'name',
+        2 => 'username',
+        3 => 'email',
         5 => 'created_at',
+    ];
+
+    protected $allowedFields = [
+        'first_name', 'last_name',
+        'email', 'username', 'password_hash', 'reset_hash', 'reset_at', 'reset_expires', 'activate_hash',
+        'status', 'status_message', 'active', 'force_pass_reset', 'permissions', 'deleted_at',
+    ];
+    protected $validationRules = [
+        'first_name'    => 'required',
+        'last_name'     => 'required',
+        'email'         => 'required|valid_email|is_unique[users.email,id,{id}]',
+        'username'      => 'required|alpha_numeric_punct|min_length[3]|max_length[30]|is_unique[users.username,id,{id}]',
+        'password_hash' => 'required',
     ];
 
     /**
@@ -21,7 +33,7 @@ class UserModel extends BaseModel
     public function getResource(string $search = ''): BaseBuilder
     {
         $builder = $this->builder()
-            ->select('id, first_name, last_name, username, email, active, created_at');
+            ->select('id, CONCAT(first_name, " ", last_name) AS name, username, email, active, created_at');
 
         $condition = empty($search)
             ? $builder
--- src/Views/User/index.php
+++ src/Views/User/index.php
@@ -6,106 +6,106 @@
 <!-- Section content -->
 <?= $this->section('content') ?>
 <!-- SELECT2 EXAMPLE -->
-<div class="card card-default">
-    <div class="card-header">
-        <div class="card-tools">
-            <div class="btn-group">
-                <a href="<?= route_to('admin/user/manage/new') ?>" class="btn btn-sm btn-block btn-primary"><i class="fa fa-plus"></i>
-                    <?= lang('boilerplate.user.add') ?>
-                </a>
+    <div class="card card-default">
+        <div class="card-header">
+            <div class="card-tools">
+                <div class="btn-group">
+                    <a href="<?= route_to('admin/user/manage/new') ?>" class="btn btn-sm btn-block btn-primary"><i class="fa fa-plus"></i>
+                        <?= lang('boilerplate.user.add') ?>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="table-responsive">
-                    <table id="table-user" class="table table-striped table-hover va-middle">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th><?= lang('boilerplate.user.fields.name') ?></th>
-                                <th><?= lang('Auth.username') ?></th>
-                                <th><?= lang('Auth.email') ?></th>
-                                <th><?= lang('boilerplate.user.fields.active') ?></th>
-                                <th><?= lang('boilerplate.user.fields.join') ?></th>
-                                <th><?= lang('boilerplate.global.action') ?></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table id="table-user" class="table table-striped table-hover va-middle">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th><?= lang('boilerplate.user.fields.name') ?></th>
+                                    <th><?= lang('Auth.username') ?></th>
+                                    <th><?= lang('Auth.email') ?></th>
+                                    <th><?= lang('boilerplate.user.fields.active') ?></th>
+                                    <th><?= lang('boilerplate.user.fields.join') ?></th>
+                                    <th><?= lang('boilerplate.global.action') ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 <!-- /.card -->
 <?= $this->endSection() ?>
 
 <?= $this->section('js') ?>
-<script>
-    var tableUser = $('#table-user').DataTable({
-        processing: true,
-        serverSide: true,
-        autoWidth: false,
-        order: [[1, 'asc']],
+    <script>
+        var tableUser = $('#table-user').DataTable({
+            processing: true,
+            serverSide: true,
+            autoWidth: false,
+            order: [[1, 'asc']],
 
-        ajax: {
-            url: '<?= route_to('admin/user/manage') ?>',
-            method: 'GET'
-        },
-        columnDefs: [{
-            orderable: false,
-            targets: [0,3,5]
-        }],
-        columns: [{
+            ajax: {
+                url: '<?= route_to('admin/user/manage') ?>',
+                method: 'GET'
+            },
+            columnDefs: [{
+                orderable: false,
+                targets: [0,4,6]
+            }],
+            columns: [{
                 'data': null
             },
-            {
-                'data': 'name'
-            },
-            {
-                'data': 'username'
-            },
-            {
-                'data': 'email'
-            },
-            {
-                'data': function(data) {
-                    return `<span class="badge ${data.active == 1 ? 'bg-success' : 'bg-danger'}">${data.active == 1 ? '<?= lang('boilerplate.user.fields.active') ?>' : '<?= lang('boilerplate.user.fields.non_active') ?>'}</span>`
-                }
-            },
-            {
-                'data': 'created_at',
-                'render': function(data) {
-                    return moment(data).fromNow()
-                }
-            },
-            {
-                "data": function(data) {
-                    return `<td class="text-right py-0 align-middle">
+                {
+                    'data': 'name'
+                },
+                {
+                    'data': 'username'
+                },
+                {
+                    'data': 'email'
+                },
+                {
+                    'data': function (data) {
+                        return `<span class="badge ${data.active == 1 ? 'bg-success' : 'bg-danger'}">${data.active == 1 ? '<?= lang('boilerplate.user.fields.active') ?>' : '<?= lang('boilerplate.user.fields.non_active') ?>'}</span>`
+                    }
+                },
+                {
+                    'data': 'created_at',
+                    'render': function (data) {
+                        return moment(data).fromNow()
+                    }
+                },
+                {
+                    "data": function (data) {
+                        return `<td class="text-right py-0 align-middle">
                             <div class="btn-group btn-group-sm">
                                 <a href="<?= route_to('admin/user/manage') ?>/${data.id}/edit" class="btn btn-primary btn-edit"><i class="fas fa-pencil-alt"></i></a>
                                 <button class="btn btn-danger btn-delete" data-id="${data.id}"><i class="fas fa-trash"></i></button>
                             </div>
                             </td>`
+                    }
                 }
-            }
-        ]
-    });
-
-    tableUser.on('draw.dt', function() {
-        var PageInfo = $('#table-user').DataTable().page.info();
-        tableUser.column(0, {
-            page: 'current'
-        }).nodes().each(function(cell, i) {
-            cell.innerHTML = i + 1 + PageInfo.start;
+            ]
         });
-    });
 
-    $(document).on('click', '.btn-delete', function(e) {
-        Swal.fire({
+        tableUser.on('draw.dt', function () {
+            var PageInfo = $('#table-user').DataTable().page.info();
+            tableUser.column(0, {
+                page: 'current'
+            }).nodes().each(function (cell, i) {
+                cell.innerHTML = i + 1 + PageInfo.start;
+            });
+        });
+
+        $(document).on('click', '.btn-delete', function (e) {
+            Swal.fire({
                 title: '<?= lang('boilerplate.global.sweet.title') ?>',
                 text: "<?= lang('boilerplate.global.sweet.text') ?>",
                 icon: 'warning',
@@ -114,34 +114,34 @@
                 cancelButtonColor: '#d33',
                 confirmButtonText: '<?= lang('boilerplate.global.sweet.confirm_delete') ?>'
             })
-            .then((result) => {
-                if (result.value) {
-                    $.ajax({
-                        url: `<?= route_to('admin/user/manage') ?>/${$(this).attr('data-id')}`,
-                        method: 'DELETE',
-                    }).done((data, textStatus, jqXHR) => {
-                        Toast.fire({
-                            icon: 'success',
-                            title: jqXHR.statusText,
-                        });
-                        tableUser.ajax.reload();
-                    }).fail((error) => {
-                        Toast.fire({
-                            icon: 'error',
-                            title: error.responseJSON.messages.error,
-                        });
-                    })
-                }
-            })
-    });
-
-    tableUser.on('order.dt search.dt', () => {
-        tableUser.column(0, {
-            search: 'applied',
-            order: 'applied'
-        }).nodes().each(function(cell, i) {
-            cell.innerHTML = i + 1;
+                .then((result) => {
+                    if (result.value) {
+                        $.ajax({
+                            url: `<?= route_to('admin/user/manage') ?>/${$(this).attr('data-id')}`,
+                            method: 'DELETE',
+                        }).done((data, textStatus, jqXHR) => {
+                            Toast.fire({
+                                icon: 'success',
+                                title: jqXHR.statusText,
+                            });
+                            tableUser.ajax.reload();
+                        }).fail((error) => {
+                            Toast.fire({
+                                icon: 'error',
+                                title: error.responseJSON.messages.error,
+                            });
+                        })
+                    }
+                })
         });
-    }).draw();
-</script>
+
+        tableUser.on('order.dt search.dt', () => {
+            tableUser.column(0, {
+                search: 'applied',
+                order: 'applied'
+            }).nodes().each(function (cell, i) {
+                cell.innerHTML = i + 1;
+            });
+        }).draw();
+    </script>
 <?= $this->endSection() ?>
